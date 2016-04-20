0866b3e6f53c7f2ab03cf7eba60044f16e71d065
sepehrvakili <sdvakili@gmail.com>
2016-04-20 16:42:10 -0700

    Implement functional-shared prompts

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,26 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {length: 0};
+  var storage = {tail: 0};
   _.extend(storage, queueMethods);
   return storage;
 };
 
 var queueMethods = {
   size: function() {
-    return this.length < 0 ? 0 : this.length;
+    return this.tail < 0 ? 0 : this.tail;
   },
   enqueue: function(value) {
-    this[this.length] = value;
-    this.length++;
+    this[this.tail] = value;
+    this.tail++;
   },
   dequeue: function() {
     var dequeued = this[0];
     for (var key in this) {
       this[key - 1] = this[key];
     }
-    delete this[this.length - 1];
-    this.length--;
+    delete this[this.tail - 1];
+    this.tail--;
     return dequeued;
   }
 };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {length: 0};
+  _.extend(storage, stackMethods);
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.length < 0 ? 0 : this.length;
+  },
+  push: function(value) {
+    this[this.length] = value;
+    this.length++;
+  },
+  pop: function() {
+    this.length--;
+    var popped = this[this.length];
+    delete this[this.length];
+    return popped;
+  }
+};
 
 
