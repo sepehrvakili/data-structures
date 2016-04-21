049cf07a012725c46199a00875a930c34cac7caf
sepehrvakili <sdvakili@gmail.com>
2016-04-21 11:22:48 -0700

    Implement linkedList and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,26 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var head = this.head.value;
+    this.head = this.head.next;
+    return head;
   };
 
   list.contains = function(target) {
+    return _.reduce(this, function(wasFound, node) {
+      return wasFound || node.value === target;
+    }, false);
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,12 +12,20 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  for (var i = 0; i < this.children.length; i++) {
+    if ( this.children[i].value === target ) {
+      return true;
+    } 
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
