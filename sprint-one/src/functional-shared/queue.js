ba5024ff17027520855a891546309c4d89f155b2
sepehrvakili <sdvakili@gmail.com>
2016-04-20 20:24:59 -0700

    Remove tail and length property assignment out of the object.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,7 +1,8 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {tail: 0};
+  var storage = {};
+  storage.tail = 0;
   _.extend(storage, queueMethods);
   return storage;
 };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,7 +1,8 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {length: 0};
+  var storage = {};
+  storage.length = 0;
   _.extend(storage, stackMethods);
   return storage;
 };
