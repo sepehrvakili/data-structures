59a0a8f3dc0b72fd6b5be39bb8503d1f9566aa57
sepehrvakili <sdvakili@gmail.com>
2016-04-22 11:41:20 -0700

    Implement hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,46 +7,50 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = [];
-  var tuple = {};
+  var callback = function(bucket, i, storage) {
+    if ( index === i ) {
+      for ( var i = 0; i < bucket.length; i++ ) {
+        if ( bucket[i][0] === k ) {
+          bucket[i][1] = v;
+        }
+      }
+    }
+  };
 
-  tuple[k] = v;
-  bucket.push(tuple);
-  this._storage.set(index, bucket);
+  if ( this.retrieve(k) ) {
+    this._storage.each(callback);
+  } else {
+    var bucket = this._storage.get(index) || [];
+    var tuple = [k, v];
+    bucket.push(tuple);
+    this._storage.set(index, bucket);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  for ( var i = 0; i < bucket.length; i++ ) {
-    if (bucket[i][k]) {
-      return bucket[i][k];
+  if ( bucket ) {
+    for ( var i = 0; i < bucket.length; i++ ) {
+      if ( bucket[i][0] === k ) {
+        return bucket[i][1];
+      }
     }
-  } 
+  }
 };
+
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
-  // [
-  //   [
-  //     {cat: happy},
-  //     {dog: sad}
-  //   ],
-  //   [
-  //     {house: brick},
-  //     {car: iron}
-  //   ]
-  // ]
-
   var callback = function(bucket, i, storage) {
-    console.log(bucket);
-    for ( var i = 0; i < bucket.length; i++ ) {
-      if ( bucket[i][k] ) {
-        bucket[i][k] = undefined;
+    if ( index === i ) {
+      for ( var i = 0; i < bucket.length; i++ ) {
+        if ( bucket[i][0] === k ) {
+          bucket[i][1] = undefined;
+        }
       }
     }
-    // array[index] = undefined;
   };
 
   this._storage.each(callback);
