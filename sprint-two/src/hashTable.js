56967d603eca9ee676381d53006e3f8c1d18acdb
sepehrvakili <sdvakili@gmail.com>
2016-04-21 22:40:07 -0700

    Made on progress on hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,50 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = [];
+  var tuple = {};
+
+  tuple[k] = v;
+  bucket.push(tuple);
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for ( var i = 0; i < bucket.length; i++ ) {
+    if (bucket[i][k]) {
+      return bucket[i][k];
+    }
+  } 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  // [
+  //   [
+  //     {cat: happy},
+  //     {dog: sad}
+  //   ],
+  //   [
+  //     {house: brick},
+  //     {car: iron}
+  //   ]
+  // ]
+
+  var callback = function(bucket, i, storage) {
+    console.log(bucket);
+    for ( var i = 0; i < bucket.length; i++ ) {
+      if ( bucket[i][k] ) {
+        bucket[i][k] = undefined;
+      }
+    }
+    // array[index] = undefined;
+  };
+
+  this._storage.each(callback);
+
 };
 
 
