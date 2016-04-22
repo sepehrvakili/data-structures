b4cc3eba3ddb6bc1def40f5bfcc88d1d4b85d32d
sepehrvakili <sdvakili@gmail.com>
2016-04-21 20:14:17 -0700

    Implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,24 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage = this._storage || [];
+  if (!_.contains(this._storage, item) ) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item) {
+  var newArray = [];
+  for ( var i = 0; i < this._storage.length; i++ ) {
+    if ( this._storage[i] !== item ) {
+      newArray.push(this._storage[i]);
+    }
+  }
+  this._storage = newArray;
 };
 
 /*
